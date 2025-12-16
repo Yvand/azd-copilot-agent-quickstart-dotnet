968fc8e1a36af305d3180771fd2491266c4c1efd
Yvand <yvandev@outlook.fr>
2025-12-16 13:15:29 +0100

    update script

--- scripts/map-env-vars.sh
+++ scripts/map-env-vars.sh
@@ -4,7 +4,7 @@
 # -o pipefail: prevents errors in a pipeline from being masked
 set -eo pipefail
 
-echo "Mapping current azd environment variables to local environment variables and Azure Web App settings..."
+echo "Mapping current azd environment variables to environment variables in the local machine and in the Azure Web App settings..."
 envVarsPrefix="M365AgentQuickstart_"
 envVarKeyTenantId="${envVarsPrefix}TokenValidation__TenantId"
 envVarKeyClientIdAudiences="${envVarsPrefix}TokenValidation__Audiences__0"
@@ -12,24 +12,33 @@ envVarKeyClientIdConnections="${envVarsPrefix}Connections__ServiceConnection__Se
 
 while IFS='=' read -r key value; do
    value=$(echo "$value" | sed 's/^"//' | sed 's/"$//')
-   if [ "$key" == "AZURE_TENANT_ID" ] ; then
-      tenantId="$value"
-      export "$envVarKeyTenantId=$value"
-   elif [ "$key" == "AZURE_RESOURCE_GROUP_NAME" ] ; then
-      rg="$value"
-   elif [ "$key" == "BOTSERVICE_CLIENT_ID" ] ; then
-      botServiceClientId="$value"
-      export "$envVarKeyClientIdAudiences=$value"
-      export "$envVarKeyClientIdConnections=$value"
-   elif [ "$key" == "WEBAPP_NAME" ] ; then
-      webAppName="$value"
-   else
-      continue
-   fi
+   case "$key" in
+      "AZURE_TENANT_ID")
+         tenantId="$value"
+         export "$envVarKeyTenantId=$value"
+         ;;
+      "AZURE_SUBSCRIPTION_ID")
+         subscription="$value"
+         ;;
+      "AZURE_RESOURCE_GROUP_NAME")
+         rg="$value"
+         ;;
+      "BOTSERVICE_CLIENT_ID")
+         botServiceClientId="$value"
+         export "$envVarKeyClientIdAudiences=$value"
+         export "$envVarKeyClientIdConnections=$value"
+         ;;
+      "WEBAPP_NAME")
+         webAppName="$value"
+         ;;
+      *)
+         continue
+         ;;
+   esac
 done <<EOF
 $(azd env get-values)
 EOF
 
-az webapp config appsettings set --name "$webAppName" --resource-group "$rg" --settings $envVarKeyTenantId=$tenantId $envVarKeyClientIdAudiences=$botServiceClientId $envVarKeyClientIdConnections=$botServiceClientId
-echo "Environment variables with prefix '$envVarsPrefix' have been set in the Web App '$webAppName' and in the local environment."
+az webapp config appsettings set --subscription "$subscription" --name "$webAppName" --resource-group "$rg" --settings $envVarKeyTenantId=$tenantId $envVarKeyClientIdAudiences=$botServiceClientId $envVarKeyClientIdConnections=$botServiceClientId > /dev/null
+echo "Environment variables with prefix '$envVarsPrefix' have been set in the Web App '$webAppName' and in the local machine."
 printenv | grep "$envVarsPrefix"
